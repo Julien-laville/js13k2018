520a862e580d58a32ff5c53a31f0ccdbe6c05482
jlaville <j.laville@qwant.com>
2018-09-03 17:36:41 +0200

    add push edge & edit edge & vertex

--- src/edge.js
+++ src/edge.js
@@ -19,9 +19,24 @@ export class Edge {
     }
     ctx.lineWidth = 5
     ctx.stroke()
+    ctx.lineWidth = 1
   }
 
-
+  de() {
+    ctx.beginPath()
+    ctx.moveTo(this.from.x * 30, this.from.y * 30)
+    ctx.lineTo(this.to.x * 30, this.to.y * 30)
+    if(this.isUnlimited) {
+      ctx.strokeStyle = "#78ff3b"
+    } else if(this.remaining === 1) {
+      ctx.strokeStyle = "#41c"
+    } else if(this.remaining === 0){
+      ctx.strokeStyle = "rgba(68,17,204,0.2)"
+    }
+    ctx.lineWidth = 5
+    ctx.stroke()
+    ctx.lineWidth = 1
+  }
 
   consume() {
     this.remaining--
--- src/editor.js
+++ src/editor.js
@@ -1,5 +1,6 @@
 import V2c from "./lib/v2c";
 import {Vertex} from "./vertex";
+import {Edge} from "./edge";
 
 export class Editor {
   constructor() {
@@ -20,12 +21,66 @@ export class Editor {
       if(this.pStance() < 6) {
         let p = new V2c(Math.floor((e.offsetX + 15) / 30) ,
           Math.floor((e.offsetY + 15) / 30))
-        this.vertices.push(new Vertex(p, this.pen))
+        let v = new Vertex(p, this.pen, 0)
+        let vfind = this.vertices.find(vertex => (vertex.pos.eq(p)))
+        if(vfind) {
+          this.currentItem = vfind
+        } else {
+          this.vertices.push(v)
+          this.currentItem = v
+        }
+        this.updateCurrent()
+      }
+      /* edges */
+
+      /* h line */
+
+      let xDiff =  this.penPos.x - Math.floor((this.penPos.x) / 30) * 30
+      let yDiff =  this.penPos.y - Math.floor((this.penPos.y + 15) / 30) * 30
+      if(xDiff > 6 && xDiff < 24 && yDiff < 6 && yDiff > -6) {
+        let fx = Math.floor((this.penPos.x) / 30)
+        let fy = Math.floor((this.penPos.y + 15) / 30)
+        let f = new V2c(fx, fy)
+        let to = new V2c(fx + 1, fy)
+        let e = new Edge(f, to, 1)
+        let efind = this.edges.find(edge => (edge.from.eq(f) && edge.to.eq(to)))
+        if(efind) {
+          this.currentItem = efind
+        } else {
+          this.edges.push(e)
+          this.currentItem = e
+        }
+
+        this.updateCurrent()
       }
 
+      /* v lines */
 
+      xDiff =  this.penPos.x - Math.floor((this.penPos.x + 15) / 30) * 30
+      yDiff =  this.penPos.y - Math.floor((this.penPos.y ) / 30) * 30
+
+      if(yDiff > 6 && yDiff < 24 && xDiff > -6 && xDiff < 6) {
+        let fx = Math.floor((this.penPos.x + 15) / 30)
+        let fy = Math.floor((this.penPos.y ) / 30)
+        let f = new V2c(fx, fy)
+        let to = new V2c(fx, fy + 1)
+        let e = new Edge(f, to, 1)
+        this.edges.push(e)
+        this.currentItem = e
+        this.updateCurrent()
+      }
 
     }
+
+    setting.onchange = () => {
+      if(this.currentItem instanceof Edge) {
+        this.currentItem.remaining = parseInt(settings.value)
+      } else {
+        this.currentItem.opt = parseInt(settings.value)
+      }
+    }
+
+
     c.onmousemove = (e) => {
       this.penPos.set(e.offsetX, e.offsetY)
     }
@@ -89,12 +144,45 @@ export class Editor {
       vertex.de()
     })
 
+    this.edges.forEach((edge) => {
+      edge.de()
+    })
+
+
+    if(this.currentItem) {
+      if(this.currentItem instanceof Edge) {
+        ctx.beginPath()
+        ctx.moveTo(this.currentItem.from.x * 30, this.currentItem.from.y * 30)
+        ctx.lineTo(this.currentItem.to.x * 30, this.currentItem.to.y * 30)
+        ctx.strokeStyle = '#ff8919'
+        ctx.stroke()
+      } else {
+        ctx.beginPath()
+        ctx.strokeStyle = '#ff8919'
+        ctx.arc(this.currentItem.pos.x * 30, this.currentItem.pos.y * 30, 8, 0 , Math.PI * 2)
+        ctx.stroke()
+      }
+    }
+
 
   }
   pStance() {
     return new V2c(Math.floor((this.penPos.x + 15) / 30) * 30,
       Math.floor((this.penPos.y + 15) / 30) * 30).stance(new V2c(this.penPos.x, this.penPos.y))
   }
+
+
+  updateCurrent() {
+    if(this.currentItem instanceof Edge) {
+      setting.value = this.currentItem.remaining
+      type.innerHTML = 'Edge'
+    } else {
+      setting.value = this.currentItem.opt
+      type.innerHTML = 'Vertex'
+    }
+
+  }
+
 
 }
 
