c337d0d358b220170971f0a8a12105181b99572e
Julien-laville <alternative451@myopera.com>
2018-09-07 22:33:32 +0200

    fix gate draw

--- src/vertex.js
+++ src/vertex.js
@@ -41,12 +41,17 @@ export class Vertex {
       ctx.fillRect(this.pos.x * 30-5, this.pos.y * 30-5, 10, 10)
     } else if(this.type === 'gate') {
         ctx.beginPath()
-        ctx.arc(this.pos.x * 30, this.pos.y * 30, 10, 0, Math.PI * 2)
+        ctx.arc(this.pos.x * 30, this.pos.y * 30, 5, 0, Math.PI * 2)
         if (p.dataCount >= this.opt) {
             ctx.strokeStyle = "#294"
+            ctx.fillStyle = "#294"
         } else {
             ctx.strokeStyle = "#911"
+            ctx.fillStyle = "#911"
         }
+        ctx.fill()
+        ctx.beginPath()
+        ctx.arc(this.pos.x * 30, this.pos.y * 30, 10, 0, Math.PI * 2)
         ctx.setLineDash([4, 2, 2, 2])
         ctx.stroke()
     } else if(this.type === 'end') {
