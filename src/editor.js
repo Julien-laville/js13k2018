2527f84aa9aa1e729e1c82a3af9b61431a877f7a
jlaville <j.laville@qwant.com>
2018-09-10 14:22:49 +0200

    moare usable éditor

--- src/editor.js
+++ src/editor.js
@@ -42,6 +42,10 @@ export class Editor {
         this.toggleButton(ttlB)
         this.pen = 'ttl'
       }
+    ttlB.onclick = () => {
+      this.toggleButton(beaconB)
+      this.pen = 'beacon'
+    }
 
 
 
